93d5a347b443b878b4e380469c15b2d27e7d3f3a
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-10-03 21:18:56 -0400

    Agregando una linea

--- ProjectBlazor/Program.cs
+++ ProjectBlazor/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
     
+    
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
